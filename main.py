e94e1448b9adbaa44e172413f52e79250c8e6138
211250165 <211250165@smail.nju.edu.cn>
2023-08-17 09:07:51 +0800

    添加对于中文的支持

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import pygame
 import sys
 import random
+import menu
 
 # 初始化Pygame
 pygame.init()
@@ -11,69 +12,81 @@ pygame.init()
 width, height = 640, 480
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("贪吃蛇游戏")
-
-# 定义方向常量
-UP = (0, -10)
-DOWN = (0, 10)
-LEFT = (-10, 0)
-RIGHT = (10, 0)
-
-# 贪吃蛇移动的方向
-direction = RIGHT
-change_to = direction
-
-# 贪吃蛇的初始位置和长度
-snake_pos = [100, 50]
-snake_body = [[100, 50], [90, 50], [80, 50]]
-
-# 食物的初始位置
-food_pos = [random.randrange(1, width//10) * 10,
-            random.randrange(1, height//10) * 10]
-food_spawn = True
+game_font = pygame.font.Font("./经典宋体简.ttf", 30)
 
 # 游戏主循环
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+def start_game(mode):
+    # 定义方向常量
+    UP = (0, -10)
+    DOWN = (0, 10)
+    LEFT = (-10, 0)
+    RIGHT = (10, 0)
 
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                change_to = UP
-            if event.key == pygame.K_DOWN:
-                change_to = DOWN
-            if event.key == pygame.K_LEFT:
-                change_to = LEFT
-            if event.key == pygame.K_RIGHT:
-                change_to = RIGHT
-                
-    # 在更新蛇的位置之前
-    if snake_pos[0] >= width:
-        snake_pos[0] = 0
-    if snake_pos[0] < 0:
-        snake_pos[0] = width
-    if snake_pos[1] >= height:
-        snake_pos[1] = 0
-    if snake_pos[1] < 0:
-        snake_pos[1] = height
+    # 贪吃蛇移动的方向
+    direction = RIGHT
+    change_to = direction
 
-    new_pos = [snake_pos[0] + change_to[0], snake_pos[1] + change_to[1]]
-    snake_body.insert(0, list(snake_pos))
-    if snake_pos == food_pos:
-        food_spawn = False
-    else:
-        snake_body.pop()
-    if not food_spawn:
-        food_pos = [random.randrange(1, width//10) * 10,
-                    random.randrange(1, height//10) * 10]
+    # 贪吃蛇的初始位置和长度
+    snake_pos = [100, 50]
+    snake_body = [[100, 50], [90, 50], [80, 50]]
+    
+    # 食物的初始位置
+    food_pos = [random.randrange(1, width//10) * 10,
+                random.randrange(1, height//10) * 10]
     food_spawn = True
+    
+    if mode == "single_player":
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
 
-    snake_pos = list(new_pos)
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        change_to = UP
+                    if event.key == pygame.K_DOWN:
+                        change_to = DOWN
+                    if event.key == pygame.K_LEFT:
+                        change_to = LEFT
+                    if event.key == pygame.K_RIGHT:
+                        change_to = RIGHT
+                        
+            # 在更新蛇的位置之前
+            if snake_pos[0] >= width:
+                snake_pos[0] = 0
+            if snake_pos[0] < 0:
+                snake_pos[0] = width
+            if snake_pos[1] >= height:
+                snake_pos[1] = 0
+            if snake_pos[1] < 0:
+                snake_pos[1] = height
 
-    screen.fill((0, 0, 0))
-    for pos in snake_body:
-        pygame.draw.rect(screen, (0, 255, 0), pygame.Rect(pos[0], pos[1], 10, 10))
-    pygame.draw.rect(screen, (255, 0, 0), pygame.Rect(food_pos[0], food_pos[1], 10, 10))
-    pygame.time.delay(50)
-    pygame.display.flip()
+            new_pos = [snake_pos[0] + change_to[0], snake_pos[1] + change_to[1]]
+            snake_body.insert(0, list(snake_pos))
+            if snake_pos == food_pos:
+                food_spawn = False
+            else:
+                snake_body.pop()
+            if not food_spawn:
+                food_pos = [random.randrange(1, width//10) * 10,
+                            random.randrange(1, height//10) * 10]
+            food_spawn = True
+
+            snake_pos = list(new_pos)
+
+            screen.fill((0, 0, 0))
+            for pos in snake_body:
+                pygame.draw.rect(screen, (0, 255, 0), pygame.Rect(pos[0], pos[1], 10, 10))
+            pygame.draw.rect(screen, (255, 0, 0), pygame.Rect(food_pos[0], food_pos[1], 10, 10))
+            pygame.time.delay(100)
+            pygame.display.flip()
+        pass
+    elif mode == "ai_battle":
+    # AI对战模式逻辑
+        pass
+    
+    # 菜单循环
+while True:
+    selected_mode = menu.main_menu()
+    start_game(selected_mode)
---
+++ menu.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+import pygame
+import sys
+
+# 初始化Pygame
+pygame.init()
+
+# 定义窗口尺寸
+width, height = 640, 480
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("贪吃蛇游戏 - 菜单")
+game_font = pygame.font.Font("./经典宋体简.ttf", 30)
+
+# 定义颜色
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+
+def draw_text(text, font, color, surface, x, y):
+    text_obj = font.render(text, True, color)
+    text_rect = text_obj.get_rect()
+    text_rect.topleft = (x, y)
+    surface.blit(text_obj, text_rect)
+
+def main_menu():
+    while True:
+        screen.fill(WHITE)
+        font = game_font
+        
+        draw_text("贪吃蛇游戏", font, GREEN, screen, 240, 100)
+        draw_text("1. 单人模式", font, RED, screen, 260, 200)
+        draw_text("2. AI对战", font, RED, screen, 260, 250)
+
+        pygame.display.update()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_1:
+                    return "single_player"
+                if event.key == pygame.K_2:
+                    return "ai_battle"
+
+if __name__ == "__main__":
+    selected_mode = main_menu()
+    print("用户选择的模式:", selected_mode)
